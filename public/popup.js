e6cc8cbf730432613d7c3c2c88d167c13488d9dd
Joshua Blount <hello@joshuablount.com>
2012-02-09 23:40:15 -0500

    small fix for IE 8

--- public/popup.js
+++ public/popup.js
@@ -38,8 +38,9 @@ displayAmericanCensorshipModal = function() {
       var css = '#signupmodal-overlay{display:block;position:absolute;top:0;left:0;width:100%;height:100%;background-color:#444;z-index:1001;-moz-opacity:.8;opacity:.8;filter:alpha(opacity=80);}#signupmodal-lightbox{display:block;position:absolute;left:15%;top:15%;width:862px;height:347px;padding:0;margin:0;background-color:#fff;z-index:1500;overflow:hidden;box-shadow:0px 0px 25px #171717;-webkit-border-radius: 3px;-moz-border-radius: 3px;border-radius: 3px;overflow:hidden;}#signupmodal-lightbox iframe{border:0;width:862px;height:527px;overflow:hidden;}#signupmodal-close{color:white;font-family:"Helvetica","Arial",sans-serif;float:right;vertical-align:10px;z-index:100;position:absolute;margin-left:800px;vertical-align:50px;}';
       var style = document.createElement('style');
       style.type = "text/css";
-      style.innerHTML = css;
-      document.body.appendChild(style);
+	$('head').append('<style type="text/css">' + css + '</style>');
+      // style.innerHTML = css;
+      // document.body.appendChild(style);
 
       // Inject HTML
       var html = '<a id="signupmodal-overlay" href="javascript:void(0)" onclick="javascript:document.getElementById(\'signupmodal-overlay\').style.display=\'none\';document.getElementById(\'signupmodal-lightbox\').style.display=\'none\';"></a>'
